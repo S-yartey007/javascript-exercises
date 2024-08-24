dcb6757a4368fe4009ab189f85215e80d5274e1c
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-08-24 17:00:18 +0000

    Finished with reversing of strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const char = string.split("")
+    let temp = "";
+    for(let i = char.length-1; i >= 0; i--) {
+             temp += char[i];
+    }
+return temp    
 
 };
 
