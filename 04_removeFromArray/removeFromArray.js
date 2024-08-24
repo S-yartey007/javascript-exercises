9f64c3ae868435f97182d625a100f6ec6760e4a1
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-08-24 17:52:43 +0000

    Finished with the remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...num) {
+  /*   array.forEach(number => {
+        if(number === num) {
+            let index = array.indexOf(num)
+            array.splice(index,1)
 
+        } else return
+
+    })
+    return array */
+ /*    for(let i = 0; i < array.length; i++) {
+        if(array[i] === num) array.splice(i,1)
+        
+    }
+    return array */
+    return array.filter(element => !num.includes(element))
 };
 
 // Do not edit below this line
