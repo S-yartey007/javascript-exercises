71a1ae20781c9d6e01a07a9cc24012acba5a9b2c
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-08-26 09:58:50 +0000

    Finished with the calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(a,b) {
+  return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  let i=0,sum=0
+  while(i < array.length) {
+    sum+= array[i];
+    i++;
+
+  }
+  return sum
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let result = 1;
+  for(let i = 0; i < array.length; i++) {
+    result*=array[i]
+    
+  }
+  return result
 
 };
 
-const power = function() {
+const power = function(a,b) {
+  return a**b;
 	
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if(a === 0) return 1;
+  let result = 1;
+  for(let i = 1; i <= a;i++) {
+    result *= i
+  }
+  return result;
 	
 };
 
