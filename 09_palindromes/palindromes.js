5b378d319ddd3daab2455b529521dea772f96dc1
Emmanuel Yartey <supremeconquerorx@gmail.com>
2024-09-07 22:13:39 +0000

    palindromes implementation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ const regExp = /\w+/g
+ let result = "";
+let pureString = string.toLowerCase().match(regExp).join("")
+for(let i = pureString.length -1 ; i >= 0; i--) {
+    result += pureString[i];
+}
+return result === pureString;
 };
 
 // Do not edit below this line
